0e2b29571fab247c3e65b6c9b7cbf76b22cc6101
bensonkuang <71754574+bensonkuang@users.noreply.github.com>
2020-11-25 14:32:58 +0800

    Add files via upload

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+    if (argc < 2 || argc > 2)
+    {
+        cout << "usage:" << endl;
+        cout << "      " << argv[0] << "user" << endl;
+    }
+    else
+    {
+        cout << "Hello," << argv[1] << "." << endl;
+        cout << "welcome to the CLI's world." << endl;
+    }
+    return 0;
+}
